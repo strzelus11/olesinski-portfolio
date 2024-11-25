01904faea4b9c92262e0f159c4a4a01298245198
strzelus11 <103840690+strzelus11@users.noreply.github.com>
2024-11-25 11:17:46 +0100

    another nextauth try

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,15 @@
 import clientPromise from "@/lib/mongodb";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { User } from "@/models/User";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import { mongooseConnect } from "@/lib/mongoose";
+import mongoose from "mongoose";
 
 export const authOptions = {
+	debug: true,
 	secret: process.env.NEXTAUTH_URL,
 	session: {
 		strategy: "jwt",
@@ -26,19 +28,31 @@ export const authOptions = {
 				password: { label: "Password", type: "password" },
 			},
 			async authorize(credentials, req) {
+				console.log("Starting mongoose connection...");
+				await mongooseConnect();
+				console.log("Mongoose connected");
+
 				const email = credentials?.email;
 				const password = credentials?.password;
 
-				await mongooseConnect();
+				console.log(`Finding user with email: ${email}`);
 				const user = await User.findOne({ email });
 
-				if (user && bcrypt.compareSync(password, user.password)) {
+				if (!user) {
+					console.log("User not found");
+					return null;
+				}
+
+				console.log("User found, validating password...");
+				if (bcrypt.compareSync(password, user.password)) {
+					console.log("Password validated, returning user");
 					return {
 						id: user._id.toString(),
 						email: user.email,
 						name: user.name,
 					};
 				} else {
+					console.log("Invalid password");
 					return null;
 				}
 			},
