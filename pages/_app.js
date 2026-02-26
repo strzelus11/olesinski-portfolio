593065f1088f13a4874d937cdecce5c68e1d3a6b
strzelus11 <103840690+strzelus11@users.noreply.github.com>
2026-02-26 21:02:13 +0100

    fixed command

--- pages/_app.js
+++ pages/_app.js
@@ -28,16 +28,15 @@ export default function App({ Component, pageProps }) {
 				async
 				src="https://www.googletagmanager.com/gtag/js?id=G-4GXLLCQ15Y"
 			></script>
-			<script>
-				dangerouslySetInnerHTML=
-				{{
+			<script
+				dangerouslySetInnerHTML={{
 					__html: `
                 window.dataLayer = window.dataLayer || []; function gtag()
 				{dataLayer.push(arguments);}
                 gtag('js', new Date()); gtag('config', 'G-4GXLLCQ15Y', { page_path: window.location.pathname });
                 `,
 				}}
-			</script>
+			></script>
 			<SessionProvider>
 				<Toaster
 					position="top-center"
